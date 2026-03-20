279f2d81a7c28667fdaae653ae7e692c05188cbe
BIT250350-Nguyen Ngoc Lien Trieu <tn0947388@gmail.com>
2026-03-20 16:15:00 +0700

    Display plot with plt.show()
    Add plt.show() to display the plot.

--- chuong 5/cau 2.py
+++ chuong 5/cau 2.py
@@ -8,6 +8,6 @@ plt.plot(x, y2, color='red', label='y = x^3')
 plt.title('Đồ thị y = x^2 và y = x^3')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.legend()  # chú thích
+plt.legend()
 plt.grid(True)
 plt.show()
